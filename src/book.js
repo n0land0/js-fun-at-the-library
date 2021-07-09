bd3024e0e97f7e3cac624ac101251d901eb7a4cc
Nolan Caine <nolancaine@gmail.com>
2021-07-08 21:06:30 -0600

    Add warm-up.js, book.js, book-test.js

--- src/book.js
+++ src/book.js
@@ -1,12 +1,51 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  var mainCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  };
+  return mainCharacter;
+}
+
+function saveReview(review, reviews) {
+  for (var i = 0; i < reviews.length; i++) {
+    if (review === reviews[i]) {
+      return;
+    }
+  }
+  reviews.push(review);
+  // return reviews;
+}
+
+function calculatePageCount(bookTitle) {
+  var bookPageCount = bookTitle.length*20;
+  return bookPageCount;
+}
+
+function writeBook(title, mainCharacter, genre) {
+  var book = {
+    title: title,
+    mainCharacter: mainCharacter,
+    pageCount: title.length*20,
+    genre: genre
+  }
+  return book;
+}
+
+function editBook(book) {
+  book.pageCount = book.pageCount*.75;
+  return book.pageCount;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
