5385bc2e8f6084ea916ff3b0b7891e5029ba7d57
Nolan Caine <nolancaine@gmail.com>
2021-07-09 23:26:33 -0600

    Add shelf.js and shelf-test.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,39 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+  }
+}
+
+function unshelfBook(bookTitle, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookTitle) {
+      shelf.splice(i, 1);
+    }
+  }
+  return shelf;
+}
+
+function listTitles(shelf) {
+  var titles = [];
+  for (var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title);
+  }
+  return(titles.join(", "));
+}
+
+function searchShelf(shelf, bookTitle) {
+  var isOnShelf = false;
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookTitle) {
+      isOnShelf = true;
+    }
+  }
+  return isOnShelf;
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
