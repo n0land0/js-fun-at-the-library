91eb2d2dbf2723b2c7c90c4b374eaf4565ca26d1
Nolan Caine <nolancaine@gmail.com>
2021-07-10 22:11:37 -0600

    Add Library.js and Library-test.js

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,46 @@
+function createLibrary(libraryName) {
+  var library = {
+    name: libraryName,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+  return library;
+};
 
+function addBook(library, book) {
+    // library.shelves.fantasy.push(book);
+    library.shelves[book.genre].push(book);
+    // library.shelves.fantasy[i];
+    // library.shelves.fiction[i];
+    // library.shelves.nonFiction[i];
+
+    // book.genre;
+
+    // var bookGenre = book.genre;
+    // if (book.genre === library.shelves["bookGenre"]) {
+    //   library.shelves["bookGenre"].push(book);
+    // }
+    // console.log(library.shelves["bookGenre"]);
+    // return library.shelves.book["genre"];
+};
+
+function checkoutBook(library, bookTitle, bookGenre) {
+  // console.log(library.shelves[bookGenre]);
+  for (var i = 0; i < library.shelves[bookGenre].length; i++) {
+    if (bookTitle === library.shelves[bookGenre][i].title) {
+      // console.log(library.shelves[bookGenre][i].title);
+      library.shelves[bookGenre].splice(i, 1);
+      return `You have now checked out ${bookTitle} from the ${library.name}`;
+    }
+  }
+  return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`;
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
